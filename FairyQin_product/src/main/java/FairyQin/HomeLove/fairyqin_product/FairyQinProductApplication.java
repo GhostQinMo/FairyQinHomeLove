a62a1896c8a70c8db68c407375f34c6422dc5a82
FairyHome <2095215135@qq.com>
2022-11-20 15:16:55 +0800

    人人管理项目没有提交成功重新提交一下

---
+++ FairyQin_product/src/main/java/FairyQin/HomeLove/fairyqin_product/FairyQinProductApplication.java
@@ -0,0 +1,13 @@
+package FairyQin.HomeLove.fairyqin_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairyQinProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FairyQinProductApplication.class, args);
+    }
+
+}
---
+++ FairyQin_product/src/test/java/FairyQin/HomeLove/fairyqin_product/FairyQinProductApplicationTests.java
@@ -0,0 +1,13 @@
+package FairyQin.HomeLove.fairyqin_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairyQinProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
